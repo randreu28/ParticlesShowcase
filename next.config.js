339b04d945cd6d08461c5ce801cc9493ad300331
randreu28 <randreu28@gmail.com>
2022-05-31 17:17:05 +0200

    Adds initial  configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+module.exports = {
+  reactStrictMode: false,
+  /* For Three.js glsl importation */
+  webpack: (config, options) => {
+      config.module.rules.push({
+          test: /\.(glsl|vs|fs|vert|frag)$/,
+          use: ['raw-loader', 'glslify-loader'],
+      });
 
-module.exports = nextConfig
+      return config;
+  },
+};
